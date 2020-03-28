86d4e82bcc41791f5069c70b09ae6dc08c02b1e6
Jamie Thompson <jlthompson76@users.noreply.github.com>
2020-03-28 10:59:42 -0400

    Modify script.js file

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 // how to create an object literal
+
 const book = {
     title: "Harry Potter",
     author: "JK Rowling",
     numPages: 250,
     genre: ["science fiction", "fiction"],
-    hasRead: false
+    hasRead: true,
+    iLoveBook: function() {
+        console.log(`I love ${this.title}!`);
+    }
 }
 
 // how to create an object constructor
@@ -21,3 +25,29 @@ console.log(book.author);
 // access song object using bracket notation
 
 console.log(song["name"]);
+
+// add a new key/value pair
+
+console.log(book.howManyCopiesSold = 500000000);
+
+// update a value
+
+console.log(book.hasRead = false);
+
+// delete a key/value
+
+delete book.numPages;
+console.log(book);
+
+// create a method
+
+book.sayHello = function() {
+    console.log(`Hello ${book.author}`);
+    console.log(`Hello ${this.author}`); // both work
+}
+
+book.sayHello();
+
+// initialize a method
+
+book.iLoveBook();
